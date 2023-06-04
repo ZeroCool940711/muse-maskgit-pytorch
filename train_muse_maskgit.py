e646f18e9342b73e0d30c3970d762c14aa5245d0
ZeroCool940711 <ZeroCool940711@users.noreply.github.com>
2023-06-03 23:15:18 -0700

    Changed the default value for the --depth argument and fixed the step counter again for the maskgit training code.

--- train_muse_maskgit.py
+++ train_muse_maskgit.py
@@ -69,7 +69,7 @@ def parse_args():
         default=1024,
         help="The sequence length. Must be equivalent to fmap_size ** 2 in vae",
     )
-    parser.add_argument("--depth", type=int, default=2, help="The depth of model")
+    parser.add_argument("--depth", type=int, default=4, help="The depth of model")
     parser.add_argument(
         "--dim_head", type=int, default=64, help="Attention head dimension"
     )
